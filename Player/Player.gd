49d3d2fd95c97d95894ef1a7add34c00e1535b23
gsw945 <gsw945@foxmail.com>
2021-02-05 12:47:38 +0800

    animationTree

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,8 @@ const MAX_SPEED = 180 # 限速
 const FRICITION = 500 # 摩擦力
 
 onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = $AnimationTree.get("parameters/playback")
 
 var velocity = Vector2.ZERO
 
@@ -15,13 +17,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		if input_vector.x > 0:
-			animationPlayer.play("RunRight")
-		else:
-			animationPlayer.play("RunLeft")
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		animationPlayer.play("IdleRight")
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICITION * delta)
 	
 	velocity = move_and_slide(velocity)
